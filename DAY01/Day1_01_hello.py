f37b785026379fc684cf6235a2caea09ac56fda9
kang kayeon <ka030202@kookmin.ac.kr>
2020-07-08 13:01:38 +0900

    print문 연습

---
+++ DAY01/Day1_01_hello.py
@@ -0,0 +1,33 @@
+# # def add(a,b):
+# #     return a+b
+
+# # print(add(3,4))
+# print("hello\n강가연입니다")
+
+# a = 273
+# b = 10.0+20.0
+# str Number
+# Number = "35"
+# print(type(a))
+# print(type(b))
+# print(type(Number))
+
+
+print("hello'python'")
+i=10
+print("hello \'python\'", i , "strt") # 다른 언어와 같이 역슬래쉬 써서 표현하라, 문자열은 ""이것으로 통일하라
+# 이스케이프 문자 역슬래시 기호와 함께함
+# \" : 큰따옴표를 의미
+# \' : 작은따옴표를 의미
+def addInt(num1, num2):
+    add_num = num1+num2
+    print(add_num)
+
+addInt(2,3)
+name = "홍길동"
+age = 24
+
+print('이름은 {0}이고, 나이는 {1}입니다'.format(name,age))
+
+print("hello의 문자 갯수")
+print(len("hello"))
